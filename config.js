cd6feb52d691c2793e816dd64cf886a0b2d906fd
Victor <vsingh57@myseneca.ca>
2024-08-19 18:50:31 -0400

    Changed next.config

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const DEBUG = 0;
 
 const API_URL = ["https://victorsingh.ca/api/", "http://localhost:3001/"];
 
-const FRONTEND_URL = ["https://victorsingh.ca:3002", "http://localhost:3002"];
+const FRONTEND_URL = ["https://v2-frontend-victor.vercel.app/", "http://localhost:3002"];
 
 module.exports = {
   API_URL,
--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ import { DEBUG } from './config';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: DEBUG === 0 ? '/keebgram' : ''
 };
 
 export default nextConfig;
