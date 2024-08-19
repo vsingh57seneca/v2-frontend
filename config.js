d2aab1653a523a221cf69419146b301963a4e5dc
Victor <vsingh57@myseneca.ca>
2024-08-19 14:14:45 -0400

    Config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+DEBUG = 0;
+
+const API_URL = [
+    "https://victorsingh.ca/api/",
+    "http://localhost:3001/"
+];
+
+const FRONTEND_URL = [
+    "https://victorsingh.ca:3002",
+    "http://localhost:3002",
+]
